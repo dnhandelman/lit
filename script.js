947ae2bf1bee05d6bd5f18d88d1a18458b8ed3ca
Daniel <dnhandelman@gmail.com>
2022-08-11 09:09:21 -0700

    fixing yes answer in prompt

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-
 function promptMe() {
     let button1 = prompt("Do you have a soul?");
-    if (button1 == ("yes")) {
+    if (button1 == "yes" || button1 == "Yes" || button1 == "yeah") {
         alert("Congratulations, please enjoy."),
         window.location = "./Poem1/index.html";
     } else {
         alert("Entrance denied.");
     }
-}
 
+    // #f00f00
+}
+/*
+cool extensions
+
+git lens
+
+auto rename tag
+
+prettier - code formatter (removes semicolons)
+
+shellcheck
+
+headwind - helps with tailwind
+
+tailwind css intellisense
+
+color highlight
+*/
