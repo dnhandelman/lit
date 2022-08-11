41fd5c7abb6cae6bf5e6d9016cee1299e12ee6b2
Daniel <dnhandelman@gmail.com>
2022-08-10 19:37:00 -0700

    Committing new page structure

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function promptMe()
 {
  let button1 = prompt("Do you have a soul?");
  if (button1 == ("yes")) {alert("Congratulations, please enjoy."), 
-    window.location = "/americanspirits.html";
+    window.location = "./Poem1/index.html";
 } else {
     alert("Entrance denied.");
     }
