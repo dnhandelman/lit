2bd50ae89f4336aa30d52c7b495ea09dc19804ee
Daniel <dnhandelman@gmail.com>
2022-08-10 20:46:51 -0700

    Fixed link 2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-function promptMe()
-{
- let button1 = prompt("Do you have a soul?");
- if (button1 == ("yes")) {alert("Congratulations, please enjoy."), 
-    window.location = "./Poem1/index.html";
-} else {
-    alert("Entrance denied.");
+function promptMe() {
+    let button1 = prompt("Do you have a soul?");
+    if (button1 == ("yes")) {
+        alert("Congratulations, please enjoy."),
+        window.location = "./Poem1/index.html";
+    } else {
+        alert("Entrance denied.");
     }
-} 
+}
 
